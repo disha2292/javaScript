00ae96240b79a148845d686a4c0421f5650a081f
disha2292 <disha2292@icloud.com>
2024-02-11 10:17:22 +0530

    again idk why

--- DataTypes/datatypes.js
+++ DataTypes/datatypes.js
@@ -1,4 +1,4 @@
-//javascript is dynamically typed language.
+//javascript is dynamically typed and weakely typed language.
 //Variables in JavaScript are not directly associated with any particular value type, and any variable can be assigned (and re-assigned) values of all types.
 
 // let a = 5;
@@ -25,7 +25,7 @@ g. BigInt: Using BigInts in JavaScript allows for precise representation and man
 
 Immutability is the state where values are immutable (that is, not able to be changed). A value is immutable when altering it is impossible. Primitive data types are immutable, as we discussed above.
 
-stored in stack 
+stored in stack memory
 */
 
 // let num = 46;
@@ -63,3 +63,85 @@ stored in stack
 
 //Brookes
 //Halina
+
+/*
+Composite Data Types ( non primitive types):
+Composite data types are used to group multiple values and treat them as a single unit. They are passed by reference.
+
+a. Object: Represents a collection of key-value pairs, where keys are strings (or symbols) and values can be of any data type, including other objects.
+
+b. Array: Represents an ordered collection of values. Arrays can hold values of any data type, including other arrays or objects.
+
+c. Function: Functions are a special type of object that can be invoked to perform a task. They can be assigned to variables, passed as arguments, and returned from other functions.
+
+d. Date: Represents a date and time value.
+
+e. RegExp: Represents a regular expression, used for pattern matching within strings.
+
+f. Map, Set, WeakMap, WeakSet: Introduced in ECMAScript 6, these are specialized data structures for storing collections of data.
+
+stored in heap memory
+/*
+Null in JavaScript means an empty value and is also a primitive type in JavaScript. The variable which has been assigned as null contains no value. Undefined, on the other hand, means the variable has been declared, but its value has not been assigned.
+
+Both undefined and null are falsy by default. So == returns true. But when we use the strict equality operator (===) which checks both type and value, since undefined and null are of different types (from the typeof Operator section), the strict equality operator returns false
+*/
+
+/*
+The Number type has only one value with multiple representations: 0 is represented as both -0 and +0 (where 0 is an alias for +0). In practice, there is almost no difference between the different representations; for example, +0 === -0 is true. However, you are able to notice this when you divide by zero:
+
+console.log(42 / +0); // Infinity
+console.log(42 / -0); // -Infinity
+
+NaN ("Not a Number") is a special kind of number value that's typically encountered when the result of an arithmetic operation cannot be expressed as a number. It is also the only value in JavaScript that is not equal to itself.
+*/
+
+
+// String
+const firstName = 'disha';
+
+console.log(firstName);
+
+// Number
+const age = 30;
+const temp = 98.9;
+
+// Boolean
+const hasKids = true;
+
+// Null
+const aptNumber = null;
+
+// Undefined
+// let score;
+const score = undefined;
+console.log(score);
+
+// Symbol
+const id = Symbol('id');
+console.log(id);
+
+// BigInt
+// const n = 9007199254740991 n;
+
+// Reference Types
+
+const numbers = [1, 2, 3, 4];
+
+const person = {
+    name: 'Brad',
+};
+
+function sayHello() {
+    console.log('Hello');
+}
+
+const output = sayHello;
+
+console.log(output, typeof output);
+
+// More info on why typeof null == object
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof#typeof_null
+
+//  More info on the "function object" type
+// https://262.ecma-international.org/5.1/#sec-11.4.3
