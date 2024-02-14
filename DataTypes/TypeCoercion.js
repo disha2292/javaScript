dbe850c80c685e31a339f99b60eefa1d1677e044
Disha Vala <116883804+disha2292@users.noreply.github.com>
2024-02-14 19:08:40 +0530

    Update TypeCoercion.js

--- DataTypes/TypeCoercion.js
+++ DataTypes/TypeCoercion.js
@@ -7,28 +7,38 @@ JavaScript is a weakly typed language. This means that you can often use a value
 let x;
 
 // Coerced to a string
-x = 5 + '5';
+// x = 5 + '5';
+// x = 5 * '5';
+// x = 5 + null;
+// x = Number(null);
 
-x = 5 + Number('5');
+// x = Number(true);
+// x = Number(false);
 
-// Coerced to a number
-x = 5 * '5';
+// // true is coerced to a 1
+// x = 5 + true;
 
-// null is coerced to 0 as it is a `falsy` value
-x = 5 + null;
+// // false is coerced to 0 (falsy)
+// x = 5 + false;
 
-x = Number(null);
-
-x = Number(true);
-x = Number(false);
-
-// true is coerced to a 1
-x = 5 + true;
-
-// false is coerced to 0 (falsy)
-x = 5 + false;
-
-// Undefined is coerced to 0 (falsy)
+// // Undefined is coerced to 0 (falsy)
 x = 5 + undefined;
 
-console.log(x, typeof x);
+
+console.log(x);
+
+// x = 5 + Number('5');
+
+// // Coerced to a number
+
+
+// // null is coerced to 0 as it is a `falsy` value
+
+
+
+
+// console.log(x, typeof x);
+
+// let str = "5";
+// let num = str * 2; // Implicit conversion of str to a number
+// console.log(num); // Output: 10
